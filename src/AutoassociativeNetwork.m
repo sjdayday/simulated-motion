0a89f124561142d504e6a5ce1baaea228c3694a8
Steve Doubleday <stevedoubleday@gmail.com>
2015-11-08 19:41:28 -0800

    cleanup comments

--- src/AutoassociativeNetwork.m
+++ src/AutoassociativeNetwork.m
@@ -1,19 +1,8 @@
-% AutoassociativeHebbMarrNetwork class:  models a network of neurons that implements
-% auto-association as proposed in: 
+% AutoassociativeNetwork class:  extends HebbMarrNetwork
+% for auto-association by adding recurrent connections from outputs 
+% from period t as the inputs to period t+1, as proposed in: 
 % McNaughton, B. L., & Nadel, L. (1990). "Hebb-Marr networks and the neurobiological
 % representation of action in space" Neuroscience and connectionist theory, 1{63.
-% The model defines three types of neurons: 
-% detonator:  output from these neurons is sufficiently great that a single input neuron
-%  has a high probability of causing an output neuron to fire.  Granule
-%  cells in dentate gyrus have such characteristics.  Inputs from these
-%  cells are termed "inputY", following the usage in the article.
-% modifiable:  output from these neurons form synapses modifiable via
-%  Hebb's rule.  These are "typical" excitatory connections, as found in the 
-%  dendrites of hippocampal pyramidal cells.  Inputs from these cells are
-%  termed "inputX".
-% inhibitory:  these neurons perform an integer division of the inputs to a
-%  principal cell, such that the principal cell fires only if the number of
-%  inputs it has received is equal to the sum of the inputX vector.
 classdef AutoassociativeNetwork < HebbMarrNetwork 
 
     properties
