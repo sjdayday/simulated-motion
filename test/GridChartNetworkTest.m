fce3b11026190300c27bd545d98cba93155d9b6d
Steve Doubleday <stevedoubleday@gmail.com>
2016-01-13 11:09:25 -0800

    comment GridChartNetworkTest.m

--- test/GridChartNetworkTest.m
+++ test/GridChartNetworkTest.m
@@ -1,15 +1,15 @@
 classdef GridChartNetworkTest < AbstractTest
     methods (Test)
-        function testCreateNetwork(testCase)
-            network = GridChartNetwork(6,5);
-            for ii = 1:100
-                for jj = 1:10
-                    network.step()
-                    if jj == 10
-                        network.plot()
-                    end
-                end
-            end
-        end
+%         function testCreateNetwork(testCase)
+%             network = GridChartNetwork(6,5);
+%             for ii = 1:100
+%                 for jj = 1:10
+%                     network.step()
+%                     if jj == 10
+%                         network.plot()
+%                     end
+%                 end
+%             end
+%         end
     end
 end
