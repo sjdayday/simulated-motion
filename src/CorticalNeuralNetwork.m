4400b84e24815b1b295c4edbc0586beaac10b7bc
Steve Doubleday <stevedoubleday@gmail.com>
2016-05-06 17:06:30 -0700

    CorticalNeuralNetwork rebuilds and generates function

--- src/CorticalNeuralNetwork.m
+++ src/CorticalNeuralNetwork.m
@@ -42,6 +42,36 @@ classdef CorticalNeuralNetwork < handle
            obj.currentExecution = [in;out]; 
            obj.executions = [obj.executions,obj.currentExecution]; 
         end
-     
+        function resetSeed(~)
+           load '../rngDefaultSettings';
+           rng(rngDefault);                
+        end
+        function net = rebuildNetwork(obj)
+           resetSeed(obj); 
+           % adapted directly from nprtool simple script output
+           %   simIn - input data.
+           %   simTg - target data.
+
+           x = obj.input;
+           t = obj.output;
+           % Choose a Training Function
+           trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
+           % Create a Pattern Recognition Network
+           hiddenLayerSize = obj.numberHiddenLayer;
+           net = patternnet(hiddenLayerSize);
+           % Setup Division of Data for Training, Validation, Testing
+           net.divideParam.trainRatio = 80/100;
+           net.divideParam.valRatio = 10/100;
+           net.divideParam.testRatio = 10/100;
+           net.trainParam.showWindow=0;  % suppress nntraintool window
+
+           % Train the Network
+           [net,~] = train(net,x,t);
+           % from the advanced script:
+           % Generate MATLAB function 
+           genFunction(net,obj.neuralNetworkFunctionName,'MatrixOnly','yes',...
+               'ShowLinks','no');
+
+        end
     end
 end
--- test/CorticalNeuralNetworkTest.m
+++ test/CorticalNeuralNetworkTest.m
@@ -44,6 +44,23 @@ classdef CorticalNeuralNetworkTest < AbstractTest
             testCase.assertEqual(neuralNetwork.currentExecution, ...
                 [2;1;0;0;0;2], 'append [in;out]');             
         end
+        function testRebuildNetworkAndGenerateFunctionFile(testCase)
+            neuralNetwork = CorticalNeuralNetwork('sim',10); 
+            in = [1;1;0;0]; 
+            out = [0;1]; 
+            in2 = [0;0;1;1]; 
+            out2 = [1;0]; 
+%   No network created unless there are at least two different cases
+            neuralNetwork.add(in,out);
+            neuralNetwork.add(in2,out2);
+            net = neuralNetwork.rebuildNetwork();             
+            net2 = neuralNetwork.rebuildNetwork();                         
+            testCase.assertEqual(net, net2, ...
+                'reset random generator each time to start from same place');             
+            testCase.assertEqual(exist([neuralNetwork.neuralNetworkFunctionName,'.m'], ...
+                'file'), 2, ...
+                '2 = exists; function file created in current directory');             
+        end
 
     end
 end
