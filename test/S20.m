8d98d5135d4a80f5f4057286e597e3bfa79a1ee0
sjdayday <stevedoubleday@gmail.com>
2020-05-18 06:58:33 -0700

    prep to run v1.0.7 runs to correct settling error

--- test/S20.m
+++ test/S20.m
@@ -7,24 +7,79 @@ classdef S20 < handle
     end
     methods 
         function runAll(obj)
-            obj.ec.sparseOrthogonalizingNetwork = true; 
-            obj.ec.ripples = 6;
-            obj.ec.nGridGains = 3; % x 2 = grids
-            obj.ec.nHeadDirectionCells = 60;
-            obj.ec.nCueIntervals = 60;
-            obj.ec.hdsMinimumVelocity = pi/30; 
-            obj.ec.minimumTurnVelocity=pi/30;
+             obj.flavors(20, 2, 1, true, true); 
+             obj.flavors(20, 4, 1, true, true); 
+             obj.flavors(20, 6, 1, true, true);             
+%             obj.flavors(20, 2, 2, true, true); 
+%             obj.flavors(20, 4, 2, true, true); 
+%             obj.flavors(20, 6, 2, true, true);             
+%             obj.flavors(20, 2, 3, true, true); 
+%             obj.flavors(20, 4, 3, true, true); 
+%             obj.flavors(20, 6, 3, true, true);             
+%             obj.flavors(20, 2, 1, false, true); 
+%             obj.flavors(20, 4, 1, false, true); 
+%             obj.flavors(20, 6, 1, false, true);             
+%             obj.flavors(20, 2, 2, false, true); 
+%             obj.flavors(20, 4, 2, false, true); 
+%             obj.flavors(20, 6, 2, false, true);             
+%             obj.flavors(20, 2, 3, false, true); 
+%             obj.flavors(20, 4, 3, false, true); 
+%             obj.flavors(20, 6, 3, false, true);             
+%             obj.flavors(20, 2, 1, true, false); 
+%             obj.flavors(20, 4, 1, true, false); 
+%             obj.flavors(20, 6, 1, true, false);             
+%             obj.flavors(20, 2, 2, true, false); 
+%             obj.flavors(20, 4, 2, true, false); 
+%             obj.flavors(20, 6, 2, true, false);             
+%             obj.flavors(20, 2, 3, true, false); 
+%             obj.flavors(20, 4, 3, true, false); 
+%             obj.flavors(20, 6, 3, true, false);             
+%             obj.flavors(20, 2, 1, false, false); 
+%             obj.flavors(20, 4, 1, false, false); 
+%             obj.flavors(20, 6, 1, false, false);             
+%             obj.flavors(20, 2, 2, false, false); 
+%             obj.flavors(20, 4, 2, false, false); 
+%             obj.flavors(20, 6, 2, false, false);             
+%             obj.flavors(20, 2, 3, false, false); 
+%             obj.flavors(20, 4, 3, false, false); 
+%             obj.flavors(20, 6, 3, false, false);             
+
+%             obj.ec.sparseOrthogonalizingNetwork = true; 
+%             obj.ec.ripples = 6;
+%             obj.ec.nGridGains = 2; % x 2 = grids
+% %             obj.ec.nHeadDirectionCells = 60;
+% %             obj.ec.nCueIntervals = 60;
+% %             obj.ec.hdsMinimumVelocity = pi/30; 
+% %             obj.ec.minimumTurnVelocity=pi/30;
+%             nextScenario = 1; % > 1 is restart after previous problem  
+%             lastScenario = 1; 
+%             obj.ec.runScenarios(nextScenario, lastScenario, 3000); 
+%             disp(obj.ec.environment.showGridSquares()); 
+        end        
+        
+        function flavors(obj, last, ripples, gains, sparse, moreCells)
+            obj.buildController(); 
+            obj.ec.sparseOrthogonalizingNetwork = sparse; 
+            obj.ec.ripples = ripples;
+            obj.ec.nGridGains = gains; % x 2 = grids
+            if moreCells
+                obj.ec.nHeadDirectionCells = 60;
+                obj.ec.nCueIntervals = 60;
+                obj.ec.hdsMinimumVelocity = pi/30; 
+                obj.ec.minimumTurnVelocity=pi/30;                
+            end
             nextScenario = 1; % > 1 is restart after previous problem  
-            lastScenario = 20; 
+            lastScenario = last; 
             obj.ec.runScenarios(nextScenario, lastScenario, 3000); 
             disp(obj.ec.environment.showGridSquares()); 
-        end        
-        function obj = S20(visual)
+        end
+
+        function buildController(obj)
             close all;
             obj.ec = ExperimentController(); 
             obj.ec.scenarioDelay = 5; % 5 seconds between delays to avoid NPE between PetriNetRunner threads
             obj.ec.report = true; 
-            obj.ec.reportTag = 'v1.0.6';
+            obj.ec.reportTag = 'v1.0.7';
             obj.ec.reportPipeTag = 'pipe-core--2.0.0'; 
             obj.ec.reportFilepath =  '../test/logs/';
 %             formattedDateTime = char(datetime(2020,1,19,16,0,55, 'Format','yyyy-MM-dd--HH-mm-ss')); 
@@ -56,7 +111,12 @@ classdef S20 < handle
 %             obj.ec.runScenarios(1, 3000); 
 % %             obj.ec.totalSteps = 10; % 28
             disp(obj.ec.environment.showGridSquares()); 
+            
         end
+        function obj = S20()
+            obj.buildController(); 
+        end
+        
         function run(obj, steps)
 %             obj.ec.runHeadDirectionSystemForSteps(steps);            
         end
