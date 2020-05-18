916337c7dc1816824a2ecf3431248d3e21553162
sjdayday <stevedoubleday@gmail.com>
2020-05-18 06:02:57 -0700

    update S14Test to match change in S14 from 895bb119da0

--- test/S14Test.m
+++ test/S14Test.m
@@ -9,11 +9,11 @@ classdef S14Test < AbstractTest
 %             placeList = script.ec.animal.hippocampalFormation.placeListDisplay;
             testCase.assertEqual(script.ec.time, 20);
             testCase.assertThat(script.ec.animal.vertices, ...
-                IsEqualTo([1.4103 0.9817; 1.3515 0.9008; 1.5427 0.8237], 'Within', RelativeTolerance(.001))); 
+                IsEqualTo([1.41169 1.005; 1.3710 0.9136; 1.5740 0.8779], 'Within', RelativeTolerance(.001))); 
 %                 IsEqualTo([1.5420 1.1020; 1.5525 1.0025; 1.7462 1.0732], 'Within', RelativeTolerance(.001))); 
             % was [1.4580 1.2933; 1.5080 1.2067; 1.6562 1.3500] at pi/6
-            testCase.assertEqual(script.ec.animal.hippocampalFormation.headDirectionSystem.getMaxActivationIndex(), 26);
-            testCase.assertEqual(script.ec.animal.hippocampalFormation.placeOutputIndices(), [41 98]); 
+            testCase.assertEqual(script.ec.animal.hippocampalFormation.headDirectionSystem.getMaxActivationIndex(), 27);
+            testCase.assertEqual(script.ec.animal.hippocampalFormation.placeOutputIndices(), [41 95]); 
         end       
 
     end
